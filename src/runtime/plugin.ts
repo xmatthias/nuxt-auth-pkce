500669ecfe08e024883eef8a28f9485a649d366a
Matthias <xmatthias@outlook.com>
2025-12-01 10:09:25 +0100

    fix: add dynamic route for auth-redirect

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,7 +1,16 @@
 import { useAuth } from './composables/auth'
-import { addRouteMiddleware, defineNuxtPlugin, navigateTo } from '#app'
+import type { ModuleOptions } from '~/src/module'
+import { addRouteMiddleware, useRuntimeConfig, defineNuxtPlugin, navigateTo, useRouter } from '#app'
 
 export default defineNuxtPlugin((_nuxtApp) => {
+  const moduleConfig = useRuntimeConfig().public.authPkce as ModuleOptions['public']
+  const redirectURI = moduleConfig.entra?.redirectUri || '/'
+  if (redirectURI !== '/') {
+    // Define a route for the redirect URI to avoid 404s
+    const router = useRouter()
+    router.addRoute({ path: redirectURI, name: 'auth-redirect', redirect: '/' })
+  }
+
   addRouteMiddleware('auth', async (to) => {
     if (to.hash.startsWith('#code')) {
       // Only run if we're returning from the OAuth redirect
